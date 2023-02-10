37fa953338020b1b5960e830a919157aa5ecf782
yingrui <yingrui.f@gmail.com>
2023-02-10 22:30:02 +0800

    fix parameters problem in dataset update script

--- dataset/update_stock_trade_daily.py
+++ dataset/update_stock_trade_daily.py
@@ -36,8 +36,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--start', type=str, default=date.today().strftime('%Y-%m-%d'), help='start date')
     parser.add_argument('--end', type=str, default=date.today().strftime('%Y-%m-%d'), help='end date')
-    parser.add_argument('--adj-data', type=bool, default=False, help='whether download adj data, default is False')
-    parser.add_argument('--trade-data', type=bool, default=False, help='whether download trade data, default is False')
+    parser.add_argument('--adj-data', type=bool, default=True, help='whether download adj data, default is False')
+    parser.add_argument('--trade-data', type=bool, default=True, help='whether download trade data, default is False')
     parser.add_argument('--dividend', type=bool, default=True, help='whether download dividend data, default is False')
     opt = parser.parse_args()
 
