c74546ad249f24138af85a76ec7125daf37e9410
yingrui <yingrui.f@gmail.com>
2024-03-10 10:21:38 +0800

    control sequence of pages

--- src/pages/3_Simulation_Result_Analysis.py
+++ src/pages/3_Simulation_Result_Analysis.py
@@ -28,7 +28,7 @@ def show_simulation_result_analysis_page():
         else:
             df_list, df_trade_list, df_benefit = load_simulation_logs(portfolio_name, total_policies, follow_stocks)
             policy_id = int(selected_policy_id)
-            st.text(calculate_cagr(df_list[policy_id]))
+            st.text(calculate_cagr(policy_id, df_list[policy_id]))
             f = draw_investment_log(df_list[policy_id], xlabel='Trade Date', ylabel='Asset (unit: 10K)',
                                     title='Assets Changes Table of Policy ' + str(policy_id))
             st.pyplot(f)
@@ -40,7 +40,7 @@ def analysis_all_policies(portfolio_name, total_policies, follow_stocks):
     df_list, df_trade_list, df_benefit = load_simulation_logs(portfolio_name, total_policies, follow_stocks)
     cagr_info = []
     for policy_id in range(0, total_policies):
-        cagr_info.append(calculate_cagr(df_list[policy_id]))
+        cagr_info.append(calculate_cagr(policy_id, df_list[policy_id]))
     st.text('\n'.join(cagr_info))
 
     for policy_id in range(0, total_policies):
@@ -62,10 +62,11 @@ def get_win_loss_dataframe(df_trade_list, total_policies):
 
 def get_win_loss(policy_id, df_trade_list):
     df_trade = df_trade_list[policy_id]
-    return df_trade[['ts_code','status']].groupby(['status']).count().rename(columns={"ts_code": "policy_{0}".format(policy_id)})
+    return df_trade[['ts_code', 'status']].groupby(['status']).count().rename(
+        columns={"ts_code": "policy_{0}".format(policy_id)})
 
 
-def calculate_cagr(df):
+def calculate_cagr(policy_id, df):
     start_date = date.fromtimestamp(df.date.values[0].astype(int) / 1e9)
     end_date = date.fromtimestamp(df.date.values[-1].astype(int) / 1e9)
     years = end_date.year - start_date.year if end_date.year > start_date.year else 1
@@ -73,20 +74,25 @@ def calculate_cagr(df):
     total = df.total.values[-1]
     cagr = round_down((total / initial_total) ** (1 / years) - 1)
     return_rate = round_down(total / initial_total * 100)
-    cagr_information = 'Initial investment is {0}, after {1} years, now is {2}, return rate is {3}%, CAGR: {4}'.format(
-        initial_total, years, total, return_rate, cagr)
+    cagr_template = 'Policy-{0}: initial investment is {1}, after {2} years, now is {3}, return rate is {4}%, CAGR: {5}'
+    cagr_information = cagr_template.format(policy_id, initial_total, years, total, return_rate, cagr)
     return cagr_information
 
 
 def get_stock_benefit(policy_id, df_trade_list):
     df_trade = df_trade_list[policy_id]
-    df_benefit = df_trade[['ts_code', 'benefit']].groupby(['ts_code']).sum(['benefit']).rename(columns={"benefit": "sum_{0}".format(policy_id)})
-    df_win = df_trade[df_trade['status']=='win']
-    df_win_sum = df_win[['ts_code', 'benefit']].groupby(['ts_code']).sum(['benefit']).rename(columns={"benefit": "win_{0}".format(policy_id)})
-    df_win_count = df_win[['ts_code', 'benefit']].groupby(['ts_code']).count().rename(columns={"benefit": "w_cnt_{0}".format(policy_id)})
-    df_loss = df_trade[df_trade['status']=='loss']
-    df_loss_sum = df_loss[['ts_code', 'benefit']].groupby(['ts_code']).sum(['benefit']).rename(columns={"benefit": "loss_{0}".format(policy_id)})
-    df_loss_count = df_loss[['ts_code', 'benefit']].groupby(['ts_code']).count().rename(columns={"benefit": "l_cnt_{0}".format(policy_id)})
+    df_benefit = df_trade[['ts_code', 'benefit']].groupby(['ts_code']).sum(['benefit']).rename(
+        columns={"benefit": "sum_{0}".format(policy_id)})
+    df_win = df_trade[df_trade['status'] == 'win']
+    df_win_sum = df_win[['ts_code', 'benefit']].groupby(['ts_code']).sum(['benefit']).rename(
+        columns={"benefit": "win_{0}".format(policy_id)})
+    df_win_count = df_win[['ts_code', 'benefit']].groupby(['ts_code']).count().rename(
+        columns={"benefit": "w_cnt_{0}".format(policy_id)})
+    df_loss = df_trade[df_trade['status'] == 'loss']
+    df_loss_sum = df_loss[['ts_code', 'benefit']].groupby(['ts_code']).sum(['benefit']).rename(
+        columns={"benefit": "loss_{0}".format(policy_id)})
+    df_loss_count = df_loss[['ts_code', 'benefit']].groupby(['ts_code']).count().rename(
+        columns={"benefit": "l_cnt_{0}".format(policy_id)})
     df_benefit = df_benefit.join(df_win_sum).join(df_loss_sum).join(df_win_count).join(df_loss_count)
     return df_benefit
 
@@ -117,5 +123,5 @@ def load_simulation_logs(portfolio_name, total_policies, follow_stocks):
     return df_list, df_trade_list, df_benefit
 
 
-st.set_page_config(layout="wide", page_title="Simulation Result Analysis") # Wide layout
+st.set_page_config(layout="wide", page_title="Simulation Result Analysis")  # Wide layout
 show_simulation_result_analysis_page()
